774b44679af8b488afc6b23d8a6e741600bc0559
green-tea-end <130977835+green-tea-end@users.noreply.github.com>
2023-04-17 21:54:49 +1200

    Add files via upload

---
+++ itproger/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'BMV',
+            'age': 18,
+            'hobby': 'football'
+        }
+    }
+    return render(request, 'main/index.html', data)
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def gallery(request):
+    return render(request, 'main/gallery.html')
